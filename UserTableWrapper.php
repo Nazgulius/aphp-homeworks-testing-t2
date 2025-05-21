151f86e932751c3eccee8d79ead4ae9addde2b2e
Maksim Chalyh <nazgulius@yandex.ru>
2025-05-21 23:49:55 +0300

    task 2 in progress

--- UserTableWrapper.php
+++ UserTableWrapper.php
@@ -1,30 +1,56 @@
 <?php
+
 declare(strict_types=1);
 
 interface TableWrapperInterface
 {
-    public function insert(array $values): void;
-    public function update(int $id, array $values): array;
-    public function delete(int $id): void;
-    
-    public function get(): array;
+  public function insert(array $values): void;
+  public function update(int $id, array $values): array;
+  public function delete(int $id): void;
+
+  public function get(): array;
 }
 
 
-class UserTableWrapper
+class UserTableWrapper implements TableWrapperInterface
 {
-    private array $rows;
+  /**
+   * @param array|[column => row_value] $values
+   */
+  
+  private array $rows;
 
-    /**
-     * @param array|[column => row_value] $values
-     */
-    public function insert(array $values): void
-    {
-        $this->rows[] = $values;
-    }
+  public function __construct()
+  {
+    $this->rows = [];
+  }
 
-    public function sum(int $x, int $y): int
-    {
-      return $x + $y;
+  public function insert(array $values): void
+  {
+    $this->rows[] = $values;
+  }
+
+  public function update(int $id, array $values): array
+  {
+    $index = $id - 1; // так как id нумерация с 1
+    if (isset($this->rows[$index])) {
+      $this->rows[$index] = $values;
+      return $this->rows[$index];
     }
+    return [];
+  }
+
+  public function delete(int $id): void
+  {
+    $index = $id - 1;
+    if (isset($this->rows[$index])) {
+      unset($this->rows[$index]);
+      $this->rows = array_values($this->rows); // переупорядочить
+    }
+  }
+
+  public function get(): array
+  {
+    return $this->rows;
+  }
 }
--- tests/UserTableWrapperTest.php
+++ tests/UserTableWrapperTest.php
@@ -1,22 +1,65 @@
 <?php
 
 require_once './UserTableWrapper.php';
+
 use PHPUnit\Framework\TestCase;
 
 class UserTableWrapperTest extends TestCase
 {
-  // первый тест для проверки работы тестов
-  public function testSum()
+  private $a;
+  private $array;
+
+  protected function setUp(): void
   {
-    $a = new UserTableWrapper();
-    $result = $a->sum(2, 3);
-    $this->assertEquals(5, $result);
+    parent::setUp();
+    $this->a = new UserTableWrapper();
+    $this->array = [
+      1 => 1,
+      2 => 2,
+      3 => 3,
+    ];
   }
 
   public function testInsert()
   {
-    $a = new UserTableWrapper();
-    $result = $a->sum(2, 3);
-    $this->assertEquals(5, $result);
+    $this->a->insert($this->array);
+    $result = $this->a->get();
+    $this->assertEquals([$this->array], $result);
+  }
+
+  public function testUpdate()
+  {
+    $this->a->insert([
+      1 => 1,
+      2 => 2,
+      3 => 3,
+    ]);
+    $this->a->update(1, [5 => 5]);
+    $result = $this->a->get();
+    $expected = [
+      [5 => 5],
+    ];
+    $this->assertEquals($expected, $result);
+  }
+
+  public function testDelete()
+  {
+    $this->a->insert([
+      1 => 1,
+      2 => 2,
+      3 => 3,
+    ]);
+    // Удаляем запись с id=1
+    $this->a->delete(1);
+    $result = $this->a->get();
+    $expected = [
+      [
+        1 => 1,
+        2 => 2,
+        3 => 3,
+      ],
+    ];
+    // Обратите внимание, что удаление первой записи сместит остальные
+    $this->assertEquals($expected, $result);
   }
 }
